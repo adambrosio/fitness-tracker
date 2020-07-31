9dcb98ab3466a1cffc339367dcdd6731ad61c17f
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-07-31 19:14:23 -0400

    functioning app

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const Exercise = require("../models/exercises");
 
-module.exports = function(app) {
-    app.get("/api/workouts", (req, res) => {
+    router.get("/api/workouts", (req, res) => {
         Exercise.find({})
         .then(exercise => {
             res.json(exercise);
@@ -12,33 +11,30 @@ module.exports = function(app) {
         });
     });
 
-    app.get("api/workouts/range", (req, res) => {
+    router.get("api/workouts/range", (req, res) => {
         Exercise.find({})
         .then(exercise => {
             res.json(exercise)
         })
         .catch(err => {
-            res.statusCode(400).json(err);
+            res.json(err);
         });
     });
 
-    app.put("/api/workouts/:id", (req, res) => {
+    router.put("/api/workouts/:id", ({ body, params }, res) => {
         Exercise.findByIdAndUpdate(
-            { _id: req.params.id },
-            {
-                // The $push operator appends a specified value to an array
-                $push: { exercises: req.body}
-            },
-                { new: true }
+             params.id,
+             { $push: { exercises: body }},
+             { new: true }
         ).then(exerciseDB => {
             res.json(exerciseDB);
         })
         .catch(err => {
-            res.statusCode(400).json(err);
+            res.json(err);
         });
     });
 
-    app.post("/api/workouts", ({ body }, res) => {
+    router.post("/api/workouts", ({ body }, res) => {
         Exercise.create(body)
         .then((exerciseDB => {
             res.json.exerciseDB;
@@ -46,7 +42,5 @@ module.exports = function(app) {
             res.statusCode(400).json(err);
         });
     });
-
-}
 
 module.exports = router;
